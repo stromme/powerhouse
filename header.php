c6a2d18f2fab9baa2162b5af0f51b09f85e8d607
Josua Leonard <josua.leonard@gmail.com>
2014-04-22 17:38:11 +0700

    Update title

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<title><?=wp_title( '|', false, 'right' ).get_bloginfo()?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php wp_head(); ?>
