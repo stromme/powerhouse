2c6da530310f765e43a3a9ab9a17aea14ca9dc5f
Josua Leonard <josua.leonard@gmail.com>
2014-04-28 19:02:00 +0700

    Add modal

---
+++ templates/modal-estimate.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Description: Estimate modal.
+ *
+ * @package 
+ * @subpackage 
+ * @since 
+ */
+?>
+
+<!-- Review Modal -->
+<div class="big-modal">
+  <div class="modal fade" id="estimate">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <h3>Get an Estimate</h3>
+        </div>
+        <div class="modal-body">
+          <?php echo do_shortcode('[contact-form-7 id="101" title="Contact form 1"]'); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- / modal -->
+
