604c02a6ca43cdb16601538c9445c0ae61aeddd4
Josua Leonard <josua.leonard@gmail.com>
2014-04-28 19:39:37 +0700

    Update cta

--- templates/modal-estimate.php
+++ templates/modal-estimate.php
@@ -9,20 +9,34 @@
 ?>
 
 <!-- Review Modal -->
-<div class="big-modal">
-  <div class="modal fade" id="estimate">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-          <h3>Get an Estimate</h3>
-        </div>
-        <div class="modal-body">
-          <?php
-            $whitelist = array('127.0.0.1', '::1');
-            echo do_shortcode('[contact-form-7 id="'.(in_array($_SERVER['REMOTE_ADDR'], $whitelist)?101:66).'" title="Estimate"]');
-          ?>
-        </div>
+<div class="modal fade" id="estimate">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h3>Get an Estimate</h3>
+      </div>
+      <div class="modal-body">
+        <?php
+          $whitelist = array('127.0.0.1', '::1');
+          echo do_shortcode('[contact-form-7 id="'.(in_array($_SERVER['REMOTE_ADDR'], $whitelist)?101:66).'" title="Estimate"]');
+        ?>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="estimate-sent">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h3>Get an Estimate</h3>
+      </div>
+      <div class="modal-body">
+        Your message was sent successfully. Thanks!
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default pull-right" data-dismiss="modal">Close</button>
       </div>
     </div>
   </div>
